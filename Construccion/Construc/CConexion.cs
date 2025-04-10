ef7eabd03b15d118e8d2f7c0a950b2c58d22086b
javie <adrian.molina0801@gmail.com>
2025-04-10 11:56:48 -0400

    Agregar archivos de proyecto.

---
+++ Construccion/Construc/CConexion.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Windows.Forms;
+using Npgsql;
+
+namespace Construccion.Construc
+{
+    internal class CConexion
+    {
+        NpgsqlConnection conex = new NpgsqlConnection();
+
+        static String servidor = "localhost";
+        static String bd = "hotel_db";
+        static String usuario = "postgres";
+        static String password = "Jadrian8";
+        static String puerto = "5432";
+
+        String cadenaConexion = "server=" + servidor + ";" + "port=" + puerto + ";" + "user id=" + usuario + ";" + "password=" + password + ";" + "database=" + bd + ";";
+
+        public NpgsqlConnection establecerConexion()
+        {
+            try
+            {
+                conex.ConnectionString = cadenaConexion;
+                conex.Open();
+                MessageBox.Show("Se estableció conexión con la base de datos.");
+            }
+            catch (NpgsqlException e)
+            {
+                MessageBox.Show("No se pudo conectar a la base de datos en PostgreSQL.");
+            }
+
+            return conex;
+        }
+
+        internal bool login(string idusuario, string contrasena)
+        {
+            bool loginExitoso = false;
+
+            try
+            {
+                string query = "SELECT COUNT(*) FROM hotel.usuario WHERE idusuario = @idusuario AND contra = @contrasena";
+
+                using (NpgsqlCommand cmd = new NpgsqlCommand(query, conex))
+                {
+                    cmd.Parameters.AddWithValue("@idusuario", idusuario);
+                    cmd.Parameters.AddWithValue("@contrasena", contrasena);
+
+                    int count = Convert.ToInt32(cmd.ExecuteScalar());
+
+                    if (count > 0)
+                    {
+                        loginExitoso = true;                  }
+                }
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show("Error en el proceso de login: " + ex.Message);
+            }
+
+            return loginExitoso;
+        }
+    }
+}
---
+++ Construccion/Form1.Designer.cs
@@ -0,0 +1,176 @@
+﻿namespace Construccion
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            button1 = new Button();
+            label1 = new Label();
+            pictureBox1 = new PictureBox();
+            panel1 = new Panel();
+            textBox2 = new TextBox();
+            label4 = new Label();
+            textBox1 = new TextBox();
+            label3 = new Label();
+            label2 = new Label();
+            ((System.ComponentModel.ISupportInitialize)pictureBox1).BeginInit();
+            panel1.SuspendLayout();
+            SuspendLayout();
+            // 
+            // button1
+            // 
+            button1.BackColor = Color.LightSteelBlue;
+            button1.ForeColor = SystemColors.ActiveCaptionText;
+            button1.Location = new Point(56, 169);
+            button1.Margin = new Padding(0);
+            button1.Name = "button1";
+            button1.RightToLeft = RightToLeft.Yes;
+            button1.Size = new Size(80, 22);
+            button1.TabIndex = 0;
+            button1.Text = "LOGEATE";
+            button1.UseVisualStyleBackColor = false;
+            button1.Click += button1_Click;
+            // 
+            // label1
+            // 
+            label1.AutoSize = true;
+            label1.Location = new Point(24, 21);
+            label1.Name = "label1";
+            label1.Size = new Size(0, 15);
+            label1.TabIndex = 3;
+            label1.Click += label1_Click;
+            // 
+            // pictureBox1
+            // 
+            pictureBox1.Image = Properties.Resources.pexels_photo_531880;
+            pictureBox1.Location = new Point(0, 0);
+            pictureBox1.Name = "pictureBox1";
+            pictureBox1.Size = new Size(497, 304);
+            pictureBox1.TabIndex = 4;
+            pictureBox1.TabStop = false;
+            // 
+            // panel1
+            // 
+            panel1.BackColor = Color.Transparent;
+            panel1.BackgroundImageLayout = ImageLayout.None;
+            panel1.BorderStyle = BorderStyle.FixedSingle;
+            panel1.Controls.Add(textBox2);
+            panel1.Controls.Add(label4);
+            panel1.Controls.Add(textBox1);
+            panel1.Controls.Add(label3);
+            panel1.Controls.Add(label2);
+            panel1.Controls.Add(button1);
+            panel1.ForeColor = SystemColors.ControlLightLight;
+            panel1.Location = new Point(155, 41);
+            panel1.Name = "panel1";
+            panel1.Size = new Size(200, 219);
+            panel1.TabIndex = 5;
+            // 
+            // textBox2
+            // 
+            textBox2.Location = new Point(15, 127);
+            textBox2.Name = "textBox2";
+            textBox2.Size = new Size(160, 23);
+            textBox2.TabIndex = 5;
+            textBox2.UseSystemPasswordChar = true;
+            textBox2.TextChanged += textBox2_TextChanged_1;
+            // 
+            // label4
+            // 
+            label4.AutoSize = true;
+            label4.ForeColor = SystemColors.ActiveCaptionText;
+            label4.Location = new Point(3, 109);
+            label4.Name = "label4";
+            label4.Size = new Size(84, 15);
+            label4.TabIndex = 4;
+            label4.Text = "CONTRASEÑA";
+            label4.Click += label4_Click;
+            // 
+            // textBox1
+            // 
+            textBox1.Location = new Point(15, 83);
+            textBox1.Name = "textBox1";
+            textBox1.Size = new Size(160, 23);
+            textBox1.TabIndex = 3;
+            textBox1.TextChanged += textBox1_TextChanged_1;
+            // 
+            // label3
+            // 
+            label3.AutoSize = true;
+            label3.ForeColor = SystemColors.ActiveCaptionText;
+            label3.Location = new Point(3, 65);
+            label3.Name = "label3";
+            label3.Size = new Size(92, 15);
+            label3.TabIndex = 2;
+            label3.Text = "USUARIO/EMAL";
+            label3.Click += label3_Click;
+            // 
+            // label2
+            // 
+            label2.AutoSize = true;
+            label2.Font = new Font("Segoe UI Black", 12F, FontStyle.Bold, GraphicsUnit.Point, 0);
+            label2.ForeColor = SystemColors.ActiveCaptionText;
+            label2.Location = new Point(25, 26);
+            label2.Name = "label2";
+            label2.Size = new Size(150, 21);
+            label2.TabIndex = 1;
+            label2.Text = "INICIO DE SESION";
+            label2.UseMnemonic = false;
+            // 
+            // Form1
+            // 
+            AutoScaleDimensions = new SizeF(7F, 15F);
+            AutoScaleMode = AutoScaleMode.Font;
+            ClientSize = new Size(495, 301);
+            Controls.Add(panel1);
+            Controls.Add(label1);
+            Controls.Add(pictureBox1);
+            Icon = (Icon)resources.GetObject("$this.Icon");
+            MaximizeBox = false;
+            Name = "Form1";
+            Text = "INICIO DE SESION";
+            ((System.ComponentModel.ISupportInitialize)pictureBox1).EndInit();
+            panel1.ResumeLayout(false);
+            panel1.PerformLayout();
+            ResumeLayout(false);
+            PerformLayout();
+        }
+
+        #endregion
+
+        private Button button1;
+        private Label label1;
+        private PictureBox pictureBox1;
+        private Panel panel1;
+        private Label label2;
+        private Label label4;
+        private TextBox textBox1;
+        private Label label3;
+        private TextBox textBox2;
+    }
+}
